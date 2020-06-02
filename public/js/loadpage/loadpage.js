bdb170a72c76806a2e5b4790393056032ec71a70
toilahieu1410 <34303013+toilahieu1410@users.noreply.github.com>
2020-06-02 15:01:40 +0700

    update code 02062020

--- public/js/loadpage/loadpage.js
+++ public/js/loadpage/loadpage.js
@@ -2,7 +2,7 @@ function onReady(callback) {
     var intervalID = window.setInterval(checkReady, 500);
 
     function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
+        if (document.getElementsByClassName('main-panel')[0] !== undefined) {
             window.clearInterval(intervalID);
             callback.call(this);
         }
@@ -10,7 +10,7 @@ function onReady(callback) {
 }
 
 function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
+    document.getElementById(id).style.display = value ? 'flex' : 'none';
 }
 
 onReady(function () {
